7f467fff0f4aceaf2733d2e61981e1e948bdfd1f
r.alejandroalcocervega <slevin.v77@gmail.com>
2018-04-12 23:13:10 -0400

    adding whackamole class and main class

--- src/main/java/org/minions/devfund/alejandro/MainWhackAMole.java
+++ src/main/java/org/minions/devfund/alejandro/MainWhackAMole.java
@@ -2,49 +2,51 @@ package org.minions.devfund.alejandro;
 import java.util.Scanner;
 
 
-class MainWhackAMole {
+public abstract class  MainWhackAMole {
     /**
-     * This is the main class to run the game.
+     * Public class for the main.
      * @param args arguments.
      */
 
-    public static void main(String[] args) {
-        /*
-          This is the main method to run the game flow.
-         */
+    public static void main(final String[] args) {
+
+        final int attempts = 50;
+        final int dimensions = 10;
+        final int amountOfMoles = 10;
+        final int exitNumber = -1;
         Scanner in = new Scanner(System.in);
-        WhackAMole whackAMole = new WhackAMole(50,10);
+        WhackAMole whackAMole = new WhackAMole(attempts, dimensions);
+        whackAMole.setMoles(amountOfMoles);
+        whackAMole.fillMatrix();
+
         boolean keepPlaying = true;
         whackAMole.printGridToUser();
-        while (keepPlaying){
+        while (keepPlaying) {
             System.out.print("Enter x Value:  ");
             int x = in.nextInt();
             System.out.print("Enter y Value:  ");
             int y = in.nextInt();
-            if (x == -1 && y == -1){
+            if (x == -1 && y == -1) {
                 System.out.println("The game has end with you surrendering,...");
                 whackAMole.printGrid();
                 keepPlaying = false;
-            }
-            else if ((x>9 || x<-1) || (y>9 || y<-1)){
+            } else if ((x > dimensions - 1 || x < exitNumber) || (y > dimensions - 1 || y < exitNumber)) {
                 System.out.println("Out of boundaries try again");
-            }
-            else {
-                whackAMole.whack(x,y);
+            } else {
+                whackAMole.whack(x, y);
                 whackAMole.printGridToUser();
-                System.out.println("You have: " + whackAMole.attemptsLeft + "attempts");
-                System.out.println("Moles left: " + whackAMole.molesLeft);
+                System.out.println("You have: " + whackAMole.getAttemptsLeft() + "attempts");
+                System.out.println("Moles left: " + whackAMole.getMoleLeft());
             }
-            if(whackAMole.attemptsLeft == 0){
+            if (whackAMole.getAttemptsLeft() == 0) {
                 System.out.println("You do not have more attempts");
                 keepPlaying = false;
             }
-
-            if(whackAMole.molesLeft == 0){
-                System.out.println("You have won the game congratulations!!!!");
+            if (whackAMole.getMoleLeft() == 0) {
+                System.out.println("You have won the game with a score of: "
+                        + whackAMole.getScore() + "congratulations!!!!");
                 keepPlaying = false;
             }
         }
-
     }
 }
--- src/main/java/org/minions/devfund/alejandro/WhackAMole.java
+++ src/main/java/org/minions/devfund/alejandro/WhackAMole.java
@@ -1,46 +1,65 @@
 package org.minions.devfund.alejandro;
 
 public class WhackAMole {
+    /**
+     * Whack a mole class.
+     */
 
-    public int score, molesLeft, attemptsLeft;
-    private char [][] moleGrid;
-    private char hiddenMole = 'm';
-    private char mole = 'M';
-    private char nothing = '*';
-    private char mode;
+    private int score;
+    private int molesLeft;
+    private int attemptsLeft;
+    private int moles;
+    private char[][] moleGrid;
+    private int size;
+    private final char mole = 'M';
+    private final char nothing = '*';
 
-    public WhackAMole(int numAttempts, int gridDimension){
-        /*
-          Constructor for the game, specifies total number of whacks allowed,
-          and the grid dimension. After reading through the assignment,
-          make sure to initialize the moleGrid with the appropriate character.
-         */
+    public WhackAMole(int numAttempts, int gridDimension) {
         this.moleGrid = new char[gridDimension][gridDimension];
-        this.molesLeft = 10;
         this.attemptsLeft = numAttempts;
         this.score = 0;
-        fillMatrix(gridDimension);
+        this.size = gridDimension;
     }
 
-    private void fillMatrix(int size) {
-        /*
-          This method will fill the grid with random placed moles.
-         */
+    private boolean place(int x, int y) {
+        if (this.moleGrid[x][y] == mole) {
+            return false;
+        } else {
+            this.moles--;
+            this.moleGrid[x][y] = mole;
+            return true;
+        }
+    }
+
+    public void whack(int x, int y) {
+        char whack = 'W';
+        if (this.moleGrid[x][y] == mole) {
+            this.moleGrid[x][y] = whack;
+            this.score++;
+            this.molesLeft--;
+            this.attemptsLeft--;
+        } else {
+            this.attemptsLeft--;
+        }
+    }
+
+    public void setMoles(int moles) {
+        this.moles = moles;
+        this.molesLeft = moles;
+    }
+
+    public void fillMatrix() {
         fillGrid();
         fillMoles(size);
     }
 
     private void fillMoles(int size) {
-        /*
-          This method fill the grid with hidden moles.
-         */
         for (int i = 0; i < size; i++) {
             boolean p = false;
             while (!p) {
                 int randomX = (int) (Math.random() * size + 0);
                 int randomY = (int) (Math.random() * size + 0);
                 if (place(randomX, randomY)) {
-                    this.moleGrid[randomX][randomY] = hiddenMole;
                     p = true;
                 }
             }
@@ -48,9 +67,6 @@ public class WhackAMole {
     }
 
     private void fillGrid() {
-        /*
-          This method fill the grid with *.
-         */
         for (int i = 0; i < moleGrid.length; i++) {
             for (int y = 0; y < moleGrid.length; y++) {
                 moleGrid[i][y] = nothing;
@@ -58,49 +74,13 @@ public class WhackAMole {
         }
     }
 
-
-    private boolean place(int x, int y) {
-        /*
-          Given a location, place a hiddenMole at that location.
-          Return true if you can. (Also update number of moles left.)
-         */
-        return this.moleGrid[x][y] != hiddenMole;
-    }
-
-    public void whack(int x, int y) {
-        /*
-          Given a location, take a whack at that location.
-          If that location contains a hiddenMole, the score,
-          number of attemptsLeft, and molesLeft is updated.
-          If that location does not contain a hiddenMole, only attemptsLeft is updated.
-         */
-        char whack = 'W';
-        if(!place(x,y)){
-            this.moleGrid[x][y] = mole;
-            this.score++;
-            this.attemptsLeft--;
-            this.molesLeft--;
-
-        }
-        else {
-            this.moleGrid[x][y] = whack;
-            this.attemptsLeft--;
-        }
-    }
-
     private void grid(char mode) {
-        /**
-         * This method draw the grid whether is for keep playing or finalize the game.
-         */
+
         for (char[] aMoleGrid : moleGrid) {
             for (int y = 0; y < moleGrid.length; y++) {
-                if (mode == 'u' && (aMoleGrid[y] == hiddenMole)){
+                if (mode == 'u' && (aMoleGrid[y] == mole)) {
                     System.out.print(" " + nothing + " ");
-                }
-                else {
-                    if(aMoleGrid[y]==hiddenMole) {
-                        aMoleGrid[y] = mole;
-                    }
+                } else {
                     System.out.print(" " + aMoleGrid[y] + " ");
                 }
             }
@@ -109,22 +89,24 @@ public class WhackAMole {
     }
 
     public void printGridToUser() {
-        /**
-         * Print the grid without showing where the moles are.
-         * For every spot that has recorded a “whacked hiddenMole,” print out a W, or * otherwise.
-         */
-        this.mode = 'u';
+        char mode = 'u';
         grid(mode);
     }
 
     public void printGrid() {
-        /**
-         * Print the grid completely. This is effectively dumping the 2d array on the screen.
-         * The places where the moles are should be printed as an ‘M’.
-         * The places where the moles have been whacked should be printed as a ‘W’.
-         * The places that don’t have a hiddenMole should be printed as *.
-         */
-        this.mode = 'i';
+        char mode = 'i';
         grid(mode);
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public int getMoleLeft() {
+        return this.molesLeft;
+    }
+
+    public int getAttemptsLeft() {
+        return this.attemptsLeft;
     }
 }

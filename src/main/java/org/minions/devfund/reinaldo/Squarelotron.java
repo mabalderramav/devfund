1f293b45afb3711b815421134699ebf2bbf963dc
reinaldo <reinaldo@cerebro.com>
2018-04-23 04:44:56 -0400

    Squarelotron

---
+++ src/main/java/org/minions/devfund/reinaldo/Squarelotron.java
@@ -0,0 +1,7 @@
+package org.minions.devfund.reinaldo;
+
+/**
+ * Created by reinaldo on 19-04-18.
+ */
+public class Squarelotron {
+}
---
+++ src/test/java/org/minions/devfund/reinaldo/SquarelotronTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by reinaldo on 19-04-18.
+ */
+public class SquarelotronTest {
+
+}

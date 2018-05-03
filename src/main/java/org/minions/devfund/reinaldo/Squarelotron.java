03ac93b5ae643391d8bd12a70f0a54fab7ee67b2
rAguilarJala <reinaldo.aguilar@jalasoft.com>
2018-05-03 12:15:26 -0400

    tast2-squarelotron

---
+++ src/main/java/org/minions/devfund/reinaldo/Squarelotron.java
@@ -0,0 +1,189 @@
+package org.minions.devfund.reinaldo;
+
+/**
+ * Class the handle the squarelotron logic.
+ */
+public class Squarelotron {
+    private int[][] squarelotron;
+    private int size;
+
+    /**
+     * @param n size to squarelotron.
+     */
+    public Squarelotron(final int n) {
+        size = n;
+        squarelotron = new int[n][n];
+        fillSquarelotron();
+    }
+
+    /**
+     * Gets the squarelotron left.
+     *
+     * @return current squarelotron value.
+     */
+    public int[][] getSquarelotron() {
+        return squarelotron.clone();
+    }
+
+    /**
+     * This method filling the squarelotron.
+     */
+    public void fillSquarelotron() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                squarelotron[i][j] = size * i + j + 1;
+            }
+        }
+    }
+
+    /**
+     * The methods get the size.
+     *
+     * @return size.
+     */
+    public int getSizeSquare() {
+        return size % 2 == 0 ? size / 2 : (size - 1) / 2;
+    }
+
+    /**
+     * The methodos swap to last and first column on the matrix.
+     *
+     * @param squarelotronCopy matrix copy.
+     * @param j                position column.
+     * @param ring             the ring.
+     */
+    public void moveTheFirstAndLasColummn(final Squarelotron squarelotronCopy, final int j, final int ring) {
+        for (int i = ring - 1; i < getSizeSquare(); i++) {
+
+            int temp = squarelotronCopy.squarelotron[i][j];
+            squarelotronCopy.squarelotron[i][j] = squarelotronCopy.squarelotron[size - 1 - i][j];
+            squarelotronCopy.squarelotron[size - 1 - i][j] = temp;
+        }
+    }
+
+    /**
+     * The methodos swap to distints last and first column on the matrix.
+     *
+     * @param squarelotronCopy matrix copy.
+     * @param j                position column.
+     * @param ring             the ring.
+     */
+    public void moveTheDistintSFirstAndLasColummn(final Squarelotron squarelotronCopy, final int j, final int ring) {
+        squarelotronCopy.squarelotron[ring - 1][j] = squarelotron[size - ring][j];
+        squarelotronCopy.squarelotron[size - ring][j] = squarelotron[ring - 1][j];
+    }
+
+    /**
+     * Method up side down flip.
+     *
+     * @param ring ring.
+     * @return squarelotron matrix.
+     */
+    public Squarelotron upsideDownFlip(final int ring) {
+        Squarelotron squarelotronCopy = new Squarelotron(size);
+        for (int columnJ = ring - 1; columnJ <= size - ring; columnJ++) {
+
+            if (columnJ == ring - 1 || columnJ == size - ring) {
+                moveTheFirstAndLasColummn(squarelotronCopy, columnJ, ring);
+            } else {
+                moveTheDistintSFirstAndLasColummn(squarelotronCopy, columnJ, ring);
+            }
+        }
+
+        return squarelotronCopy;
+    }
+
+    /**
+     * methods move diagonal right.
+     *
+     * @param squarelotronCopy matrix copy.
+     * @param j                position column.
+     * @param ring             move the ring.
+     */
+    public void moveDiagonalRight(final Squarelotron squarelotronCopy, final int j, final int ring) {
+        for (int i = ring - 1; i <= size - ring; i++) {
+            if (j != i) {
+                squarelotronCopy.squarelotron[i][j] = squarelotron[j][i];
+                squarelotronCopy.squarelotron[j][i] = squarelotron[i][j];
+            }
+        }
+    }
+
+    /**
+     * methods move diagonal Left.
+     *
+     * @param squarelotronCopy matrix copy.
+     * @param j                position column.
+     * @param ring             move the Left.
+     */
+    public void moveDiagonalLeft(final Squarelotron squarelotronCopy, final int j, final int ring) {
+        for (int i = size - ring; i >= ring; i--) {
+            if (j != i) {
+                squarelotronCopy.squarelotron[i][j] = squarelotron[j][i];
+                squarelotronCopy.squarelotron[j][i] = squarelotron[i][j];
+            }
+        }
+
+    }
+
+    /**
+     * Diagonals Flip of the squarelotron.
+     *
+     * @param ring for move.
+     * @return squarelotron.
+     */
+    public Squarelotron mainDiagonalFlip(final int ring) {
+        Squarelotron squarelotronCopy = new Squarelotron(size);
+        moveDiagonalRight(squarelotronCopy, ring - 1, ring);
+        moveDiagonalLeft(squarelotronCopy, size - ring, ring);
+        return squarelotronCopy;
+    }
+
+    /**
+     * Rotates the squarelotron.
+     *
+     * @param numberOfTurns Integer that represent the moves number.
+     */
+    public void rotateRight(int numberOfTurns) {
+        while (numberOfTurns != 0) {
+            if (numberOfTurns > 0) {
+                rotateRightClockWise();
+                numberOfTurns--;
+            } else {
+                rotateLeftClockWise();
+                numberOfTurns++;
+            }
+        }
+    }
+
+    /**
+     * Methods that rotateRightClockWise.
+     */
+    public void rotateRightClockWise() {
+
+        final int[][] squarelotronClone = new int[size][getSquarelotron()[0].length];
+
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                squarelotronClone[i][j] = squarelotron[(size - 1) - j][i];
+            }
+        }
+        squarelotron = squarelotronClone.clone();
+    }
+
+    /**
+     * Methods that rotateLeftClockWise.
+     */
+    public void rotateLeftClockWise() {
+
+        final int[][] squarelotronClone = new int[getSquarelotron()[0].length][size];
+
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                squarelotronClone[i][j] = squarelotron[j][(size - 1) - i];
+            }
+        }
+
+        squarelotron = squarelotronClone.clone();
+    }
+}

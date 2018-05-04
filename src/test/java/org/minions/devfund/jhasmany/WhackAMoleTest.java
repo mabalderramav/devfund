b105d9b5bc33e885c03b66fa038a0a703ed5ed66
tumael <tumael.suscripciones@gmail.com>
2018-05-04 01:50:00 -0400

    Added the test related to WhackAMole class

--- src/test/java/org/minions/devfund/jhasmany/WhackAMoleTest.java
+++ src/test/java/org/minions/devfund/jhasmany/WhackAMoleTest.java
@@ -8,41 +8,69 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class WhackAMoleTest {
-	private WhackAMole whackAMole;
-	
-	@Before
+    private WhackAMole whackAMole;
+
+    @Before
     public void setup() {
-		this.whackAMole = new WhackAMole(4, 7);
+	this.whackAMole = new WhackAMole(4, 3);
     }
-	
-	@Test
+
+    @Test
     public void testFreePlace() {
-        assertTrue("The place isn't free.", this.whackAMole.place(0, 0));
+	assertTrue("The place isn't free.", this.whackAMole.place(0, 0));
     }
-	
-	@Test
+
+    @Test
     public void testOccupiedPlace() {
-		this.whackAMole.place(0, 0);
-        assertFalse("The place is free.", this.whackAMole.place(0, 0));
+	this.whackAMole.place(0, 0);
+
+	assertFalse("The place is free.", this.whackAMole.place(0, 0));
     }
-	
-	@Test
+
+    @Test
     public void testOutOfRangeWhack() {
-        this.whackAMole.whack(10, 10);
-        assertEquals("The coordinates are valid.", 0, this.whackAMole.getAttemptsLeft());
+	this.whackAMole.whack(10, 10);
+	int expectedResult = 0;
+	int actualResult = this.whackAMole.getAttemptsLeft();
+
+	assertEquals("The coordinates are valid.", expectedResult, actualResult);
     }
-	
-	@Test
+
+    @Test
     public void testRightWhack() {
-		this.whackAMole.place(0, 0);
-        this.whackAMole.whack(0, 0);
-        assertEquals("The whack is wrong.", 'W', this.whackAMole.getMoleGrid()[0][0]);
+	this.whackAMole.place(0, 0);
+	this.whackAMole.whack(0, 0);
+	char expectedTesult = 'W';
+	char actualResult = this.whackAMole.getMoleGrid()[0][0];
+
+	assertEquals("The whack is wrong.", expectedTesult, actualResult);
     }
-	
-	@Test
+
+    @Test
     public void testWrongWhack() {
-		this.whackAMole.place(0, 0);
-        this.whackAMole.whack(1, 0);
-        assertEquals("The whack is right.", 3, this.whackAMole.getAttemptsLeft());
+	this.whackAMole.place(0, 0);
+	this.whackAMole.whack(1, 0);
+	int expectedResult = 3;
+	int actualResult = this.whackAMole.getAttemptsLeft();
+
+	assertEquals("The whack is right.", expectedResult, actualResult);
+    }
+
+    @Test
+    public void testPutTheMoles() {
+	this.whackAMole.putTheMoles(3);
+	int expectedResult = 3;
+	int actualResult = this.whackAMole.getMolesLeft();
+
+	assertEquals("There is a difference with moles", expectedResult, actualResult);
+    }
+
+    @Test
+    public void testForPutTheMolesIncorrect() {
+	this.whackAMole.putTheMoles(3);
+	int expectedResult = 2;
+	int actualResult = this.whackAMole.getMolesLeft();
+
+	assertFalse("There isn't a difference with moles", expectedResult == actualResult);
     }
 }
